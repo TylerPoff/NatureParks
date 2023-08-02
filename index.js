d435b4b9a4e32863ed1222d4a9acfa73043f59ce
TylerPoff <Tyler.Poff1900@gmail.com>
2023-08-01 20:48:24 -0400

    Fill in index.js and srver.js placeholders

---
+++ index.js
@@ -0,0 +1,32 @@
+import mongodb from 'mongodb';
+import dotenv from 'dotenv';
+import app from './server.js';
+import TripsDAO from './dai/tripsDAO.js';
+import ListsDAO from './dao/listsDAO.js';
+
+async function main() {
+    dotenv.config();
+
+    const client = new mongodb.MongoClient(
+        proecss.env.PARKS_DB_URI
+    );
+    const port = process.env.PORT || 8000;
+
+    try {
+        await client.connect();
+        await TripsDAO.injectDB(client);
+        await ListsDAO.injectDB(client);
+
+        app.listen(port, () => {
+            console.log(`Server is running on ${port}`);
+        });
+    }
+    catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+main().catch(console.error);
+
+export default app;
---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import parks from './api/parks.route.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/parks', parks);
+app.use('*', (req, res) => {
+    res.status(404).json({error: 'not found'});
+});
+
+export default app;
