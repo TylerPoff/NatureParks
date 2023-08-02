a8e014365314439f8452147991de1e27a6a4b9d6
TylerPoff <Tyler.Poff1900@gmail.com>
2023-08-01 21:12:47 -0400

    Add outline of routes

--- api/parks.route.js
+++ api/parks.route.js
@@ -1,6 +1,19 @@
 import express from 'express';
 import TripsController from './trips.controller.js';
 import ListsController from './lists.controller.js';
+import ParksController from './parks.controller.js';
 
 const router = express.Router();
 
+router.route('/parklist').get(ParksController.apiGetParks);
+router.route('/parklist/:id').get(ParksController.apiGetParkByID);
+
+router.route('/trips').get(TripsController.apiGetTrips);
+router.route('/trips').put(TripsController.apiUpdateTrip);
+router.route('/trips').delete(TripsController.apiDeleteTrip);
+
+router.route('/lists').get(ListsController.apiGetLists);
+router.route('/lists').put(ListsController.apiUpdateList);
+router.route('/lists').delete(ListsController.apiDeleteList);
+
+export default router;
